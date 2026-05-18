885ae36c56fce9fe3197ccb619d038b9a4dd52bd
dupliivladyslav <0204dvv@gmail.com>
2026-05-18 15:54:44 +0300

    add third try

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,9 @@
 const images = [
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/orchids-4202820__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/orchids-4202820_1280.jpg",
     description: "Hokkaido Flower",
   },
   {
